7f50d770adb6283c79d9272aea71ab5cb024aa54
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2024-05-07 11:29:19 -0300

    chore: updating calls to proxies

--- src/test/recon/Properties.sol
+++
@@ -1,8 +0,0 @@
-
-// SPDX-License-Identifier: GPL-2.0
-pragma solidity ^0.8.0;
-
-import {Asserts} from "@chimera/Asserts.sol";
-import {Setup} from "./Setup.sol";
-
-abstract contract Properties is Setup, Asserts {}
